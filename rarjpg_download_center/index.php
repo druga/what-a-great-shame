f25666cbe72a136629e6f61f23d455529ddf0366
dukzcry <dukzcry@users.noreply.github.com>
2011-12-15 20:26:42 +0400

    Add LICENSE.

--- rarjpg_download_center/index.php
+++ rarjpg_download_center/index.php
@@ -1,4 +1,11 @@
 <?
+	/* Written by Artem Falcon <lomka@gero.in> */
+	/* This program is free software. It comes without any warranty, to
+	 * the extent permitted by applicable law. You can redistribute it
+	 * and/or modify it under the terms of the Do What The Fuck You Want
+	 * To Public License, Version 2, as published by Sam Hocevar. See
+	 * http://sam.zoy.org/wtfpl/COPYING for more details. */
+
 	ini_set('error_reporting', E_NONE);
 	
 	//Путь к папке со скриптом относительно корня сайта
