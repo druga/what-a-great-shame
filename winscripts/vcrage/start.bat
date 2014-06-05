6d084f6a5e4edbd9da17ddef1c328b3dfddcb122
dukzcry <dukzcry@users.noreply.github.com>
2014-06-05 11:28:36 +0400

    Remove shit.

--- winscripts/vcrage/start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-@copy plugins\vc_main.asi* backup\
-@del plugins\vc_main.asi*
-@copy backup\Trainertbogt.asi .
-@copy backup\launchvcr.exe .
-@attrib +R trainertbogt.asi
-@launchvcr
--- winscripts/vcrage/update.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-@attrib -R trainertbogt.asi
-@del trainertbogt.asi
-@copy backup\vc_main.asi* plugins\
-@copy /Y LaunchVCR_new.exe.backup LaunchVCR.exe
-@launchvcr
